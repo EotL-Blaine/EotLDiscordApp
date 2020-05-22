347f6baf50b26af904a0c73a6523827645053a07
David Halek <dhalek0331@gmail.com>
2020-05-22 14:30:50 -0500

    telnet input loop -tell btest-

--- connect.py
+++ connect.py
@@ -13,6 +13,16 @@ PASS = os.getenv('EOTL_PASS')
 #
 # username = 'btest'
 # password = 'fatboy'
+PROMPT = b"Ready>"
+
+def get_who_list(tn):
+    print("========[ WHO LIST  ]========")
+    tn.write(b"who -ps eval -f i:3000\n")
+
+    text = tn.read_until(b" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~").decode('ascii')
+    # print(tn.read_until(b" ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~").decode('ascii'))
+    print(text)
+    print("========[  WHO END  ]========")
 
 with Telnet(HOST, PORT) as tn:
     # tn.interact()
@@ -21,16 +31,38 @@ with Telnet(HOST, PORT) as tn:
     tn.read_until(b"Password: ")
     tn.write(PASS.encode('ascii') + b"\n")
 
-    test_str = bytes(f'tell {USER} hi\n', 'utf-8')
-    tn.write(test_str)
-    tn.write(b"tell btest hi\n")
-    tn.write(b"who -ps eval -f i:3000\n")
-    # text = tn.read_until(b'aving')
-    tn.write(b"quit\n")
+    print("========[ CONNECTED ]========")
+    connected = True
+    tn.read_until(b'The Eotl uptime meter').decode('ascii')
 
+    # This is before prompt
+    tn.read_until(PROMPT).decode('ascii')
+
+    # Start input loop
+    print("========[ LOOPING ]========")
+    while connected:
+        tn.read_until(b"***BEGIN***")
+        msg = tn.read_until(b"***END***").decode('ascii')[0:-9]
+        print(f'MESSAGE: {msg}')
+        # Todo: do command here
+        if msg == 'disconnect':
+            connected = False
+    print("========[ END LOOP ]========")
+
+    # print("after read_until(PROMPT)")
+
+    # get_who_list(tn)
+
+    # print(tn.read_until(PROMPT).decode('ascii'))
+    # print(tn.read_all().decode('ascii'))
+    test_str = bytes(f'tell {"blaine"} hi\n', 'ascii')
+    tn.write(test_str)
+    # tn.write(b"tell blaine hi\n")
+    # text = tn.read_until(b'aving')
     print(tn.read_all().decode('ascii'))
-    print("========[ ENDED ]========\n")
+    tn.write(b"quit\n")
     # print(text)
+
 
 
 # three ways to send properly coded text
